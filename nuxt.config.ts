e4b81ba94454b852e4eecdf7e705d22086771fe8
Emir Tucek <emir@tucek.me>
2024-06-14 13:26:11 +0200

    2406141326

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  //devtools: { enabled: true },
-  ssr: false,
+  devtools: { enabled: true },
+  //ssr: false,
+  nitro: {
+    static: true
+  },
   css: ['assets/css/main.css'],
   postcss: {
     plugins: {
